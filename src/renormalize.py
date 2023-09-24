6cc653fc205979c75620a97b0437793c53dda5aa
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2023-09-25 01:25:50 +0800

    ci: Pre-commit

--- src/renormalize.py
+++ src/renormalize.py
@@ -1,12 +1,15 @@
+from __future__ import annotations
+
 import logging
 from glob import glob
-from os.path import isfile, basename, splitext, join, dirname
 from os import rename
+from os.path import basename, dirname, isfile, join, splitext
+from typing import TYPE_CHECKING, NamedTuple
 
 from tqdm import tqdm
 
-from typing import NamedTuple, Optional
-from util import Entity
+if TYPE_CHECKING:
+    from util import Entity
 
 
 class Suspect(NamedTuple):
@@ -14,16 +17,13 @@ class Suspect(NamedTuple):
     entity: Entity
 
 
-def match(file_basename: str, entities: list[Entity]) -> Optional[Entity]:
+def match(file_basename: str, entities: list[Entity]) -> Entity | None:
     suspects: list[Suspect] = []
 
     for entity in entities:
         for key, value in entity.items():
             if value in file_basename:
-                suspects.append(Suspect(
-                    reason=key,
-                    entity=entity
-                ))
+                suspects.append(Suspect(reason=key, entity=entity))
                 break
 
     if len(suspects) == 0:
@@ -37,7 +37,7 @@ def match(file_basename: str, entities: list[Entity]) -> Optional[Entity]:
     return suspects[0].entity
 
 
-def format(pattern: str, entity: Entity) -> str:
+def format_basename(pattern: str, entity: Entity) -> str:
     for key, value in entity.items():
         pattern = pattern.replace(f":{key}", value)
 
@@ -49,38 +49,40 @@ class Plan(NamedTuple):
     dst: str
 
 
-def renormalize_plan(pattern: str, pathname_or_files: str | list[str], entities: list[Entity]) -> list[Plan]:
+def renormalize_plan(
+    pattern: str, pathname_or_files: str | list[str], entities: list[Entity]
+) -> list[Plan]:
     logging.debug(f"{pathname_or_files=}")
     logging.debug(f"{type(pathname_or_files)=}")
-    files = glob(pathname_or_files) if type(
-        pathname_or_files) == str else pathname_or_files
+    files = (
+        glob(pathname_or_files)
+        if isinstance(pathname_or_files, str)
+        else pathname_or_files
+    )
     logging.debug(f"{files=}")
     if len(files) == 0:
         logging.warning(f"未找到任何文件：“{pathname_or_files}”。")
 
     plans: list[Plan] = []
 
-    for src in tqdm(files, desc='Generate plans', unit='files', colour='blue'):
+    for src in tqdm(files, desc="Generate plans", unit="files", colour="blue"):
         assert isfile(src), f"“{src}”不是文件。"
 
         src_basename, ext = splitext(basename(src))
         entity = match(src_basename, entities)
 
         if entity is not None:
-            dst_basename = format(pattern, entity)
+            dst_basename = format_basename(pattern, entity)
 
             if src_basename == dst_basename:
                 logging.info(f"已符合模式：“{src}”。")
             else:
-                plans.append(Plan(
-                    src=src,
-                    dst=join(dirname(src), dst_basename + ext)
-                ))
+                plans.append(Plan(src=src, dst=join(dirname(src), dst_basename + ext)))
 
     return plans
 
 
 def execute(plans: list[Plan]) -> None:
-    for p in tqdm(plans, desc='Execute plans', unit='files', colour='green'):
+    for p in tqdm(plans, desc="Execute plans", unit="files", colour="green"):
         rename(p.src, p.dst)
         logging.info(f"Rename “{p.src}” → “{p.dst}”.")
--- src/util.py
+++ src/util.py
@@ -1,10 +1,12 @@
+from __future__ import annotations
+
 from csv import DictReader
 from typing import Any, Final
 
 Entity: Final = dict[str, Any]
 
 
-def load_entities(filename) -> list[Entity]:
-    with open(filename, 'r', encoding='utf-8') as f:
+def load_entities(filename: str) -> list[Entity]:
+    with open(filename, encoding="utf-8") as f:
         reader = DictReader(f)
-        return [row for row in reader]
+        return list(reader)
